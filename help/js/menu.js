4148f57e6a316e0a176e897544263bfe9c2fc3cd
Gilberto Tavares <camaleaun@users.noreply.github.com>
2022-08-21 19:57:42 -0300

    Update menu.js

--- help/js/menu.js
+++ help/js/menu.js
@@ -10,37 +10,37 @@ $(document).ready(function() {
 			<li> \
 				<a href="index.html" class="noselect {{? it.section == 0 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Intro</span> \
+					<span class="cd-label">Introdução</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="02-emphasis.html" class="noselect {{? it.section == 1 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Emphasis</span> \
+					<span class="cd-label">Ênfase</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="03-paragraphs.html" class="noselect {{? it.section == 2 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Paragraphs</span> \
+					<span class="cd-label">Parágrafos</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="04-headings.html" class="noselect {{? it.section == 3 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Headings</span> \
+					<span class="cd-label">Títulos</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="05-blockquotes.html" class="noselect {{? it.section == 4 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Blockquotes</span> \
+					<span class="cd-label">Citações em bloco</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="06-lists.html" class="noselect {{? it.section == 5 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Lists</span> \
+					<span class="cd-label">Listas</span> \
 				</a> \
 			</li> \
 			<li> \
@@ -52,25 +52,25 @@ $(document).ready(function() {
 			<li> \
 				<a href="08-images.html" class="noselect {{? it.section == 7 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Images</span> \
+					<span class="cd-label">Imagens</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="09-code.html" class="noselect {{? it.section == 8 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Code</span> \
+					<span class="cd-label">Código</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="10-nestedLists.html" class="noselect {{? it.section == 9 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">Nested Lists</span> \
+					<span class="cd-label">Listas aninhadas</span> \
 				</a> \
 			</li> \
 			<li> \
 				<a href="99-end.html" class="noselect {{? it.section == 10 }}is-selected{{?}}"> \
 					<span class="cd-dot"></span> \
-					<span class="cd-label">The End</span> \
+					<span class="cd-label">O fim</span> \
 				</a> \
 			</li> \
 		</ul> \
